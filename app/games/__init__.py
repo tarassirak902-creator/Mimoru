7a64f35891ba6eefe45ee65336980396b3ba1c46
tarassirak902-creator <tarassirak902@gmail.com>
2026-08-29 05:29:52 +0700

    Register Mafia in game registry

--- app/games/__init__.py
+++ app/games/__init__.py
@@ -1,3 +1,12 @@
 from app.games.registry import game_registry
+from app.games.mafia import MafiaGame, mafia_definition
 
-__all__ = ["game_registry"]
+
+def register_builtin_games() -> None:
+    if game_registry.get(mafia_definition.code) is None:
+        game_registry.register(mafia_definition, MafiaGame())
+
+
+register_builtin_games()
+
+__all__ = ["game_registry", "register_builtin_games"]
