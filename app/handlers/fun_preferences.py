572258ec2be804ad8072e3348b7ad3d9681f0154
tarassirak902-creator <tarassirak902@gmail.com>
2026-09-01 05:01:58 +0700

    Wire game center text entry

--- app/handlers/fun_preferences.py
+++ app/handlers/fun_preferences.py
@@ -4,12 +4,14 @@ from aiogram import Router
 
 from app import game_friendly_history, game_friendly_results, group_help_full
 from app.games import handlers as game_handlers
+from app.games import text_entry as game_text_entry
 from app.games.mafia import handlers as mafia_handlers
 
 
 router = Router(name=__name__)
 _INCLUDED_CALLBACK_FAMILIES = ("gm", "fsfriendly", "fshfriendly")
 router.include_router(mafia_handlers.router)
+router.include_router(game_text_entry.router)
 router.include_router(game_handlers.router)
 router.include_router(group_help_full.router)
 router.include_router(game_friendly_results.router)
