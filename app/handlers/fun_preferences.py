3373ae8cf213800cab568c85e519fb20a32aa235
tarassirak902-creator <tarassirak902@gmail.com>
2026-09-02 00:22:59 +0700

    Wire game settings subrouter

--- app/handlers/fun_preferences.py
+++ app/handlers/fun_preferences.py
@@ -4,6 +4,7 @@ from aiogram import Router
 
 from app import game_friendly_history, game_friendly_results, group_help_full
 from app.games import handlers as game_handlers
+from app.games import settings_handlers as game_settings_handlers
 from app.games import text_entry as game_text_entry
 from app.games.mafia import handlers as mafia_handlers
 
@@ -12,6 +13,7 @@ router = Router(name=__name__)
 _INCLUDED_CALLBACK_FAMILIES = ("gm", "fsfriendly", "fshfriendly")
 router.include_router(mafia_handlers.router)
 router.include_router(game_text_entry.router)
+router.include_router(game_settings_handlers.router)
 router.include_router(game_handlers.router)
 router.include_router(group_help_full.router)
 router.include_router(game_friendly_results.router)
