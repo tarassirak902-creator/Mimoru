ea79132967efa7a2e7747794c4547dec4a924fc8
tarassirak902-creator <tarassirak902@gmail.com>
2026-09-02 02:31:40 +0700

    Wire Arena router

--- app/handlers/fun_preferences.py
+++ app/handlers/fun_preferences.py
@@ -6,6 +6,7 @@ from app import game_friendly_history, game_friendly_results, group_help_full
 from app.games import handlers as game_handlers
 from app.games import settings_handlers as game_settings_handlers
 from app.games import text_entry as game_text_entry
+from app.games.arena import handlers as arena_handlers
 from app.games.battleship import handlers as battleship_handlers
 from app.games.cards import handlers as cards_handlers
 from app.games.crocodile import handlers as crocodile_handlers
@@ -13,7 +14,6 @@ from app.games.mafia import handlers as mafia_handlers
 from app.games.quiz import handlers as quiz_handlers
 from app.games.roulette import handlers as roulette_handlers
 from app.games.spy import handlers as spy_handlers
-
 
 router = Router(name=__name__)
 _INCLUDED_CALLBACK_FAMILIES = ("gm", "fsfriendly", "fshfriendly")
@@ -24,6 +24,7 @@ router.include_router(battleship_handlers.router)
 router.include_router(roulette_handlers.router)
 router.include_router(crocodile_handlers.router)
 router.include_router(cards_handlers.router)
+router.include_router(arena_handlers.router)
 router.include_router(game_text_entry.router)
 router.include_router(game_settings_handlers.router)
 router.include_router(game_handlers.router)
